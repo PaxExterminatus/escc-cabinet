d9e41cdb939114e4d4f1e19a84a09daf94e8f4ec
Voronin Vitaliy <paxexterminatus@gmail.com>
2021-09-26 22:50:51 +0300

    build

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,12 @@
 const mix = require('laravel-mix');
+const config = require('./webpack.config');
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+mix
+    .js('resources/js/app.js', 'public/js/app.js')
+    .sass('resources/sass/app.sass', 'public/css')
+    .version()
+    .vue()
+    .webpackConfig({
+        resolve: config.resolve,
+    })
+;
