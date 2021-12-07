52d282f3400403c718532ebdc94019f59115d392
Voronin Vitaliy <paxexterminatus@gmail.com>
2021-12-07 03:37:12 +0300

    Revert "fill form"
    This reverts commit b916239b688af2f853a5fa26735cdb2d814f0f9b.

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 /opt/php80/bin/php -f /usr/local/bin/composer install
 /opt/php80/bin/php -f /usr/local/bin/composer dump-autoload
-/opt/php80/bin/php artisan config:cache
--- routes/auth.php
+++ routes/auth.php
@@ -10,7 +10,8 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+    ->middleware('guest');
 
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
     ->middleware('auth')
