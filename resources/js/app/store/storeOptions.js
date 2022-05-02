42a9ab5cac2a0cec7735fe0804aea909ffeada79
Vitaliy <paxexterminatus@gmail.com>
2022-05-02 21:33:15 +0300

    VideoPlayerStore - added video player

--- resources/js/app/store/storeOptions.js
+++ resources/js/app/store/storeOptions.js
@@ -1,10 +1,12 @@
 import auth from './authModule'
-import audio from './AudioPlayer/AudioPlayerStore'
+import audio from 'cmp/AudioPlayer/AudioPlayerStore'
+import video from 'cmp/VideoPlayer/VideoPlayerStore'
 
 const storeOptions = {
     modules: {
         auth,
         audio,
+        video,
     },
     state: {},
     mutations: {},
---
+++ resources/js/components/VideoPlayer/VideoPlayerStore.js
@@ -0,0 +1,30 @@
+const state = () => ({
+    show: false,
+    list: [],
+    title: '',
+    src: '',
+});
+
+const mutations = {
+    show(store) {
+        store.show = true;
+    },
+    hide(store) {
+        store.show = false;
+    },
+    setList(store, list) {
+        store.list = list;
+    },
+    setTitle(store, title) {
+        store.title = title;
+    },
+    play(store, url) {
+        store.src = url;
+    },
+}
+
+export default {
+    namespaced: true,
+    state,
+    mutations,
+};
