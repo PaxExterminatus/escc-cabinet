d78e57b5f6cd784c0c013dbe29e500e2f8e91374
Voronin Vitaliy <PaxExterminatus@gmail.com>
2023-11-06 05:00:42 +0300

    V4 - css update

--- resources/js/components/media/LessonReader/LessonReader.vue
+++ resources/js/components/media/LessonReader/LessonReader.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="lesson-reader">
-    <iframe :src="src"/>
-  </div>
+    <div class="lesson-reader">
+        <iframe @load="onLoadFrame" id="lesson-reader-iframe" :src="src"/>
+    </div>
 </template>
 
 <script>
 
 export default {
-  name: 'LessonReader',
-  props: {
-    src: Object,
-  },
+    name: 'LessonReader',
+    props: {
+        src: String,
+    },
+    methods: {
+        onLoadFrame()
+        {
+            //const frame = document.getElementById('lesson-reader-iframe');
+            //const doc = frame.contentWindow.document;
+            //doc.getElementById('download').style.display = 'none';
+            //doc.getElementById('secondaryDownload').style.display = 'none';
+        },
+    },
 }
 </script>
